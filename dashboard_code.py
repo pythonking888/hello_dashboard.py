476f6d7117836e72e341b558fb8b6217503c2a18
pythonking888 <j_koon888@hotmail.com>
2025-08-31 19:09:15 +0100

    Add files via upload

---
+++ dashboard_code.py
@@ -0,0 +1,11 @@
+dashboard_code = """
+import streamlit as st
+
+st.set_page_config(page_title="Vishan's Dashboard", layout="centered")
+
+st.title("👋 Hi Vishan")
+st.write("This is the beginning to the end.")
+
+st.markdown("---")
+st.caption("Built with ❤️ using Streamlit")
+"""
